ed19e832b67bc574ea01bd3c6e57bb48f806189d
liuy363 <liuy363@mcmaster.ca>
2020-02-01 17:11:11 -0500

    upload UI design part

---
+++ QHacks2020/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  QHacks2020
+//
+//  Created by Jiwoo Lee on 2020-01-31.
+//  Copyright © 2020 Jiwoo Lee. All rights reserved.
+//
+import Foundation
+
+struct Constants {
+    struct Storyboard {
+        static let mainViewController = "MainVC" //reduce possibility of typo
+    }
+}
---
+++ QHacks2020/ViewControllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  QHacks2020
+//
+//  Created by Jiwoo Lee on 2020-01-30.
+//  Copyright © 2020 Jiwoo Lee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
